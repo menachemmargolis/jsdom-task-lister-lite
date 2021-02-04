302cf8587025683d095d7ff85a539fdafa1226a7
menachem margolis <margolismendel5728@gmail.com>
2021-02-03 21:21:43 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById('create-task-form')
+
+   form.addEventListener('submit', function(event){
+     event.preventDefault()
+     let ul = document.getElementById('tasks')
+
+     let list = document.createElement("li")
+     
+     
+     list.innerHTML = event.target[0].value
+
+     if(event.target[1].value == '1'){list.style.color ='red'}
+     else if(event.target[1].value == '2'){list.style.color ='blue'}
+
+
+
+     let destroy = document.createElement('button')
+     destroy.innerText = "delete"
+  
+   
+     ul.append(destroy, list)
+
+          destroy.addEventListener('click',function(event){
+            let deltedLi = event.target.nextElementSibling
+            destroy.remove()
+            deltedLi.remove()
+          })
+     
+   }) 
+
 });
